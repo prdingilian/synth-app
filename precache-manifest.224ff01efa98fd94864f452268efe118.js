635401419014909bf7b6aba8ca3881735d9cdf93
prdingilian <pr.dingilian@gmail.com>
2020-08-16 14:09:35 -0600

    Updates

---
+++ precache-manifest.224ff01efa98fd94864f452268efe118.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "d33981fa42b80420610b63dc6781cb58",
+    "url": "/synth-app/index.html"
+  },
+  {
+    "revision": "e9f5d2bd3b15296691d8",
+    "url": "/synth-app/static/css/main.6161707c.chunk.css"
+  },
+  {
+    "revision": "3c94dabac7d3cd2498a7",
+    "url": "/synth-app/static/js/2.9bdd5f23.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/synth-app/static/js/2.9bdd5f23.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "e9f5d2bd3b15296691d8",
+    "url": "/synth-app/static/js/main.a4d1784d.chunk.js"
+  },
+  {
+    "revision": "846447f2082b0bdca8d7",
+    "url": "/synth-app/static/js/runtime-main.97f42354.js"
+  }
+]);
